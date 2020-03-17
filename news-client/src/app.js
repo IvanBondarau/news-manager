57ba90c84eaacb5fb5b8fe9e30e5a6d956f4bb75
Ivan Bondarau <bondareff011@gmail.com>
2020-03-17 16:02:21 +0300

    Routing added

--- news-client/src/app.js
+++ news-client/src/app.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import Header from './header.js'
 import TagPage from './tag/tagpage.js'
+
+import {
+    Route,
+    NavLink,
+    HashRouter
+  } from "react-router-dom";
 
 export default class App extends React.Component {
 
     render() {
         return (
-                    <div class="demo-layout-waterfall mdl-layout mdl-js-layout">
-                        <Header/>
-                        <TagPage/>
+            <HashRouter>
+            <div class="demo-layout-waterfall mdl-layout mdl-js-layout">
+                <header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
+                    <div class="mdl-layout--large-screen-only mdl-layout__header-row"/>
+                    <div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
+                        <NavLink to="/" className="mdl-layout__tab">Home</NavLink>
+                        <NavLink to="/tags" className="mdl-layout__tab">Tags</NavLink>
+                        <NavLink to="/authors" className="mdl-layout__tab">Authors</NavLink>
+                        <NavLink to="/news" className="mdl-layout__tab">News</NavLink>
                     </div>
+                </header>
+                <Route path="/tags" component={TagPage}/>
+            </div>
+            </HashRouter>
                 
         )
     }
